fec703171d7e63b5cb1d0a817652aa30adef58be
w4bo <m.francia@unibo.it>
2022-11-04 09:37:41 +0100

    fix error on colormap

--- materials/run.bat
+++ materials/run.bat
@@ -1,3 +1,4 @@
+git pull
 docker build -t jupyter .
 docker run -p 8888:8888 --rm -v %cd%:/notebooks jupyter
 pause
--- materials/run.sh
+++ materials/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -exo
+git pull
 docker build -t jupyter .
 docker run -p 8888:8888 --rm -v $(pwd):/notebooks jupyter
