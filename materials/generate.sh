756a31e7b2b623b134175d9640144bc1fd2eb26c
Matteo Francia <m.francia@unibo.it>
2023-04-18 17:31:32 +0200

    fix build

--- materials/generate.sh
+++ materials/generate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -exo
 for FILE in *.ipynb; do 
+    echo "Processing $FILE file...";
     filename=$(basename -- "$FILE")
     extension="${filename##*.}"
     filename="${filename%.*}"
