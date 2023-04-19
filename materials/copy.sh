aa714b05950197a7ea1980344eadf5db1ef44e56
w4bo <m.francia@unibo.it>
2023-04-19 21:48:48 +0200

    update

--- materials/copy.sh
+++ materials/copy.sh
@@ -8,6 +8,7 @@ cp 01-DataPreprocessing.ipynb ../../2023-bbs-dm/materials/
 cp 02-MachineLearning.ipynb ../../2023-bbs-dm/materials/
 cp mytheme.css ../../2023-bbs-dm/materials/
 cp generate.sh ../../2023-bbs-dm/materials/
+cp Dockerfile ../../2023-bbs-dm/materials/
 cd ../../2023-bbs-dm/
 git add .
 git commit -m "update"
