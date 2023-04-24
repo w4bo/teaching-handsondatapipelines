7985df79d69cc956ac9df9af7b71be309b73ec2d
Matteo Francia <m.francia@unibo.it>
2023-04-24 15:45:53 +0200

    update

--- materials/copy.sh
+++ materials/copy.sh
@@ -12,7 +12,7 @@ cp generate.sh ../../2023-bbs-dm/materials/
 cp Dockerfile ../../2023-bbs-dm/materials/
 cd ../../2023-bbs-dm/
 git stash
-git pull
 git add .
 git commit -m "update"
+git pull
 git push -f
